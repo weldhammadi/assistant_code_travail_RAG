c5f11fb323330e026604ed8385438cdf04d4b794
Hakim <horairyhakim@gmail.com>
2026-07-08 11:55:43 +0200

    feature: init the vector db class

---
+++ vector_db.py
@@ -0,0 +1,41 @@
+import chromadb
+from sentence_transformers import SentenceTransformer
+from config import EMBEDDING_MODEL
+
+import os
+import pandas
+
+class VectorDB:
+	def __init__(self, vector_db_path="", chuncks=None):
+		if os.path.exists(vector_db_path):
+			self.load_vector_db(vector_db_path)
+
+
+		elif chuncks:
+			self.create_vector_db(vector_db_path, chuncks)
+
+
+	def load_vector_db(self, vector_db_path):
+		pass
+
+
+
+	def create_vector_db(self, vector_db_path, chuncks):
+		pass
+
+
+
+	def get_embeddings(self, chuncks):
+		pass
+
+
+
+	def retrieve(self, question, n=3):
+		pass
+
+
+if __name__ == "__main__":
+	vector_db_path="my_vector_db"
+	chuncks=pandas.read_csv("05_corpus_rag.csv")
+
+	vector_db_object = VectorDB(vector_db_path, chuncks)
