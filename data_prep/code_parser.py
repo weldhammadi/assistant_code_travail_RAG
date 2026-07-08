f0481c2fed3da02299bf9bca2592c46d5fdcd2b8
Manon Tessier <manon.tessier@etu.utc.fr>
2026-07-08 19:10:34 +0200

    bug-fix: npw the categorie takes the right hierarchie node

--- data_prep/code_parser.py
+++ data_prep/code_parser.py
@@ -65,7 +65,7 @@ class CodeTravailParser:
 
         num = data.get("num") or data.get("cid")
         cid = data.get("cid")
-        categorie = self._infer_categorie(hierarchy.get("partie"))
+        categorie = self._infer_categorie(hierarchy.get("partie_racine"))
         breadcrumb = self._build_breadcrumb(hierarchy)
         
         # Récupération de l'ID de la version la plus récente
