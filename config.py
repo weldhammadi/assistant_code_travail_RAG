300587fc9d2d4e067155c8239be4c797947d5a44
unknown <hamzaelmortada@gmail.com>
2026-07-08 15:17:42 +0200

    feat: modified config to add moderator

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 from dotenv import load_dotenv
 import os
+from pathlib import Path
 
 load_dotenv()
 
 
 GROQ_API_KEY=os.environ["GROQ_API_KEY"]
 
+BASE_DIR = Path(__file__).resolve().parent
 
 EMBEDDING_MODEL="distiluse-base-multilingual-cased-v2"
 LLM_MODEL="openai/gpt-oss-120b"
 MODERATOR_MODEL="openai/gpt-oss-safeguard-20b"
+MODERATOR_SYSTEM_PROMPT_PATH = BASE_DIR / "moderator_system.txt"
